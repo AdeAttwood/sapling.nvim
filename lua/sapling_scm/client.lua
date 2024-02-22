73a8acee4819d6e0b0edf8baa2488454d3d791a5
Ade Attwood <adrian.attwood@marketdojo.com>
2024-02-22 08:09:04 +0000

    feat: support showing commits and logging
    Summary:
    
    Create two new commands, "Sshow" and "Slog" they will run the sapling show and
    log commands. All the output will be put info a buffer. This is all implemented
    using the `BufReadCmd` auto command so it can be easily expanded.
    
    The show command will load the diff info a buffer with the details and
    description at the top of the buffer in comments. The file will be a diff
    filetype so its recommended to treesitter for nice diff highlighting
    
    The log command will run `sl log` with your given ref and put the output in a
    buffer. Right how we are loading the raw output of the command in the buffer,
    all highlighting is done with baleia. This will escape your ansi output and
    provide the correct colors. This uses the default log command so the output can
    be configured in sapling with `ui.logtemplate`
    
    Test Plan:
    
    Right now there is no testing for this. I will in the future set this up in CI.
    Because this project is very early I would like to have a bit more of a history
    before we start using it for testing.

--- lua/sapling_scm/client.lua
+++ lua/sapling_scm/client.lua
@@ -1,5 +1,8 @@
 local client = {}
 
+local SHOW_COMMAND = [[sl show -Tjson '%s']]
+local LOG_COMMAND = [[sl log -r '%s']]
+
 ---@class HeadInfo
 ---@field node string
 ---@field remotenames string[]
@@ -12,6 +15,32 @@ local client = {}
 client.head_info = function()
   local head_info = vim.fn.system [[sl log -r '.' -T"{dict(remotenames,node,peerurls)|json}"]]
   return vim.json.decode(head_info)
+end
+
+---@class ShowResult
+---@field node string
+---@field date table<number, number>
+---@field user string
+---@field phase string
+---@field desc string
+---@field diff string
+
+--- Gets info about a single commit
+--
+---@param commit string The commit like to show
+---@return ShowResult
+client.show = function(commit)
+  local result = vim.fn.system(SHOW_COMMAND:format(commit))
+  local decoded = vim.json.decode(result)
+
+  return decoded[1]
+end
+
+--- Runs the sapling log command and returns the output as a list of strings.
+--
+---@return string[]
+client.log_text = function(pattern)
+  return vim.fn.systemlist(LOG_COMMAND:format(pattern))
 end
 
 return client
