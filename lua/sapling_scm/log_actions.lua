85b8206d6f626c765a0035edf8e9afa5e98c5bf4
Ade Attwood <adrian.attwood@marketdojo.com>
2024-02-25 20:40:07 +0000

    feat: support metaedit and commiting
    Summary:
    
    You can not use the metaedit command and commit, from inside the nvim instance.
    This will use the sl exe and some tricks to send the file that sapling wants to
    edit back to the editor. We can then open the files up and start editing.
    
    This will work by using "<C-e>" in the log buffer to edit the message that you
    are currently on. To commit from inside the log buffer you can press "<C-c>" or
    there is the "Scommit" user command that can be used from anywhere
    
    Test Plan:
    
    I have not attempted to write test for this one yet. I feel that will be a
    little annoying as we are testing on the repo it self. I think if we want to
    test this sort of stuff we will need to clone repos like we are doing in CI.

--- lua/sapling_scm/log_actions.lua
+++ lua/sapling_scm/log_actions.lua
@@ -1,3 +1,4 @@
+local editor_command = require "sapling_scm.editor_command"
 local actions = {}
 
 ---@return string
@@ -8,6 +9,15 @@ end
 actions.show_current_hash = function()
   local hash = get_hash_from_line()
   vim.cmd("edit sl://show/" .. hash)
+end
+
+actions.metaedit = function()
+  local hash = get_hash_from_line()
+  editor_command.run(string.format("sl metaedit -r '%s'", hash))
+end
+
+actions.commit = function()
+  editor_command.run "sl commit -v"
 end
 
 return actions
