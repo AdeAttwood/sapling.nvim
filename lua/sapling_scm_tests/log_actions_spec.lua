da31fa1c340d6615047bcd2908654ee08c0da94d
Ade Attwood <adrian.attwood@marketdojo.com>
2024-03-11 18:55:12 +0000

    feat: add bookmark when viewing logs
    Summary:
    
    Now when you are on a log buffer you can use <C-b> to add a bookmark to the
    current node.
    
    Test Plan:
    
    Tests have been added and we have CI. I have also been using this for quite a
    while now locally.

---
+++ lua/sapling_scm_tests/log_actions_spec.lua
@@ -0,0 +1,56 @@
+require "sapling_scm_tests.setup"
+
+local actions = require "sapling_scm.log_actions"
+local editor_command = require "sapling_scm.editor_command"
+
+local stub = require("busted").stub
+local match = require("busted").match
+
+describe("bookmark", function()
+  vim.cmd "edit sl://show/."
+  stub(editor_command, "run")
+
+  teardown(function()
+    editor_command.run:revert()
+  end)
+
+  describe("with no bookmark added in the input", function()
+    stub(vim.ui, "input", function(_, callback)
+      callback(nil)
+    end)
+
+    teardown(function()
+      vim.ui.input:revert()
+    end)
+
+    actions.bookmark()
+
+    it("calls vim.ui.input when the bookmark is called", function()
+      assert.stub(vim.ui.input).was_called_with(match.is_table(), match.is_function())
+    end)
+
+    it("dose not call the editor_command.run", function()
+      assert.stub(editor_command.run).was_not_called()
+    end)
+  end)
+
+  describe("with a name in the callback for the input", function()
+    stub(vim.ui, "input", function(_, callback)
+      callback "MY_BOOKMARK"
+    end)
+
+    teardown(function()
+      vim.ui.input:revert()
+    end)
+
+    actions.bookmark()
+
+    it("calls vim.ui.input when the bookmark is called", function()
+      assert.stub(vim.ui.input).was_called_with(match.is_table(), match.is_function())
+    end)
+
+    it("calls the editor command to create a bookmark", function()
+      assert.stub(editor_command.run).was_called_with(match.has_match "sl bookmark")
+    end)
+  end)
+end)
