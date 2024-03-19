12affacc767876312c175b2f7c0d999d72682d7e
Ade Attwood <adrian.attwood@marketdojo.com>
2024-03-19 21:50:13 +0000

    feat: add support for sapling show urls
    Summary:
    
    When you are in a sapling show buffer, running `:Sbrowse` will now take to to
    the commit URL for the current revset. The revset will be resolved to a commit
    ID so you will get taken to the correct permalink URL.
    
    Test Plan:
    
    This has been tested manually and there are new tests to cover the new
    functionality. This covers both the new commit url format and the full
    get_url_for_file function.

--- lua/sapling_scm/remote_url.lua
+++ lua/sapling_scm/remote_url.lua
@@ -1,3 +1,6 @@
+local client = require "sapling_scm.client"
+local fs = require "sapling_scm.fs"
+
 local remote_url = {}
 
 ---@param commit_info CommitInfo
@@ -20,6 +23,38 @@ end
 remote_url.blob = function(path, ref, file, start_line, end_line)
   local host = path:gsub(".git$", "")
   return string.format("%s/blob/%s/%s/\\#L%s-L%s", host, ref, file, start_line, end_line)
+end
+
+---@param path string
+---@param ref string
+---@return string
+remote_url.commit = function(path, ref)
+  local host = path:gsub(".git$", "")
+  return string.format("%s/commit/%s", host, ref)
+end
+
+---@param file_name string
+---@param start_line number
+---@param end_line number
+---@return string
+remote_url.get_url_for_file = function(file_name, start_line, end_line)
+  local default_path = client.config "paths.default"
+  local url
+
+  local parsed = fs.parse_url(file_name)
+  if parsed and parsed.action == "show" then
+    -- The show command can take any revset to show. This will resolve the
+    -- revset into a commit hash so we can send the user to teh correct
+    -- permalink url. This will also prevent us from sending the user to a url
+    -- with a sapling revset in that that hosts don't understand.
+    local rev = client.commit_info(parsed.commit).node
+    url = remote_url.commit(default_path, rev)
+  else
+    local ref = remote_url.get_object_ref(client.commit_info())
+    url = remote_url.blob(default_path, ref, file_name, start_line, end_line)
+  end
+
+  return url
 end
 
 return remote_url
